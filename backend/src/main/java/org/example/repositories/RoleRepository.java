96c601fd8255d7df0581af38da0f425503227d1c
Andy Haag <andyhaag@gmail.com>
2024-01-06 16:44:55 -0600

    Add models and repositories for user authentication

---
+++ backend/src/main/java/org/example/repositories/RoleRepository.java
@@ -0,0 +1,16 @@
+package org.example.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import org.example.models.ERole;
+import org.example.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
+
+
